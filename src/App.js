182e56fcecfab4674964233d11c194beaacbb938
DavionChew <38470007+DavionChew@users.noreply.github.com>
2021-06-01 22:51:23 +0800

    Components and JSX learning

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
 import './App.css';
+import Info from "./Info.js";
 
 function App() {
   return (
     <div className="App">
-      <p>Bare bones app.</p>
+      <Info />
+      <AddItem />
+      <AddItem />
+      <AddItem />
     </div>
   );
 }
 
+function AddItem() {
+  const value = "default"
+  return (
+    <form>
+      <label for="text-form">Type something: </label>
+      <input type="text" value={value} id="text-form"/>
+    </form>
+  )
+}
 export default App;
---
+++ src/Info.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+class Info extends React.Component {
+    render() {
+        const title = "This is my title.";
+        const showTitle = true;
+
+        return (
+            <div>
+              <h1>{showTitle ? title : "No title"}</h1>
+              <p>Manage your stuff.</p>
+            </div>
+          );
+    }
+}
+
+export default  Info;
